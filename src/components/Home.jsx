45894b9b7f7f9f1f3c3844deb2678eb9889868cd
Sanu Kumar <145834771+sk66641@users.noreply.github.com>
2025-01-06 12:48:39 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,30 +17,18 @@ export const convertUnit = (celsius) => {
 };
 
 export const convertUnixToTime = (unixTimestamp, timezoneOffset) => {
-    const localDateTime = DateTime.fromSeconds(unixTimestamp).setZone('UTC').plus({ seconds: timezoneOffset });
+    const localDateTime = DateTime.fromSeconds(unixTimestamp).setZone('UTC').plus({seconds: timezoneOffset});
     return localDateTime.toFormat('EEE, dd MMM yyyy | hh:mm a');
 };
 
 export const convertUnixToString = (unixTimestamp, timezoneOffset) => {
-    const localDateTime = DateTime.fromSeconds(unixTimestamp).setZone('UTC').plus({ seconds: timezoneOffset });
+    const localDateTime = DateTime.fromSeconds(unixTimestamp).setZone('UTC');
     return localDateTime.toFormat('hh:mm a');
 };
 
 export const convertUnixToDate = (unixTimestamp, timezoneOffset) => {
-    const localDateTime = DateTime.fromSeconds(unixTimestamp).setZone('UTC').plus({ seconds: timezoneOffset });
-    return localDateTime.toFormat('EEE, dd MMM yyyy');
-};
-
-
-export const convertWindDirection = (deg) => {
-    const directions = [
-        "North", "North-NE", "Northeast", "East-NE",
-        "East", "East-SE", "Southeast", "South-SE",
-        "South", "South-SW", "Southwest", "West-SW",
-        "West", "West-NW", "Northwest", "North-NW"
-    ];
-    const index = Math.round(deg / 22.5) % 16;
-    return directions[index];
+    const localDateTime = DateTime.fromSeconds(unixTimestamp).setZone('UTC');
+    return localDateTime.toFormat('ZZZZ | EEE, dd MMM yyyy');
 };
 
 export const convertTimezone = (timezoneOffset) => {
@@ -49,6 +37,18 @@ export const convertTimezone = (timezoneOffset) => {
     const sign = offsetHours >= 0 ? ' +' : ' -';
     return `UTC${sign}${String(Math.abs(offsetHours)).padStart(2, '0')}:${String(offsetMinutes).padStart(2, '0')}`;
 };
+
+export const convertWindDirection = (deg) => {
+    const directions = [
+        "North", "North-NE", "Northeast", "East-NE",
+        "East", "East-SE", "Southeast", "South-SE",
+        "South", "South-SW", "Southwest", "West-SW",
+        "West", "West-NW", "Northwest", "North-NW"
+    ];
+    const index = Math.round(deg / 22.5);
+    return directions[index];
+};
+
 
 export const formatVisibility = (visibility) => (visibility / 10000).toFixed(2) + ' km';
 
